af6f321fe4b61c46d4e892878a20255ac7347e09
Armand Sallé <armand.salle@gmail.com>
2020-11-08 13:12:14 +0100

    remove cursor on touch screen

--- src/components/cursor/index.js
+++ src/components/cursor/index.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useRef } from "react"
 import { gsap } from "gsap"
+import useIsTouchDesign from "../../hooks/useIsTouchDesign"
 
 const Cursor = () => {
   const cursorRef = useRef()
+  const cursorWrapperRef = useRef()
+
   const mouse = useRef({
     x: window.innerWidth / 2,
     y: window.innerHeight / 2,
@@ -13,36 +16,45 @@ const Cursor = () => {
     y: window.innerHeight / 2,
   })
 
+  const isToucheDevice = useIsTouchDesign()
+
   useEffect(() => {
     const getMousePos = ({ clientX, clientY }) => {
       mouse.current.x = clientX
       mouse.current.y = clientY
     }
 
-    document.addEventListener("mousemove", getMousePos)
+    if (!isToucheDevice) {
+      document.addEventListener("mousemove", getMousePos)
 
-    const animate = () => {
-      requestAnimationFrame(animate)
-      const x = mouse.current.x
-      const y = mouse.current.y
+      const animate = () => {
+        requestAnimationFrame(animate)
+        const x = mouse.current.x
+        const y = mouse.current.y
 
-      prevMouse.current.x += (x - prevMouse.current.x) * 0.2
-      prevMouse.current.y += (y - prevMouse.current.y) * 0.2
+        prevMouse.current.x += (x - prevMouse.current.x) * 0.2
+        prevMouse.current.y += (y - prevMouse.current.y) * 0.2
 
-      gsap.set(cursorRef.current, {
-        x: prevMouse.current.x,
-        y: prevMouse.current.y,
-      })
+        gsap.set(cursorRef.current, {
+          x: prevMouse.current.x,
+          y: prevMouse.current.y,
+        })
+      }
+
+      animate()
+    } else {
+      cursorWrapperRef.current.style.display = "none"
     }
-
-    animate()
 
     return () => {
-      document.removeEventListener("mousemove", getMousePos)
+      if (!isToucheDevice) {
+        document.removeEventListener("mousemove", getMousePos)
+      }
     }
-  }, [])
+  }, [isToucheDevice])
+
   return (
-    <div className="cursor-wrapper">
+    <div className="cursor-wrapper" ref={cursorWrapperRef}>
       <div className="cursor" ref={cursorRef}>
         <span className="cursor__span">
           View
