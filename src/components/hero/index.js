21d1b010b0dd36cecb9dfb6dc7d509f33fa218b1
Armand Sallé <armand.salle@gmail.com>
2020-12-08 10:42:50 +0100

    reveal animation hero

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,9 +1,39 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
+import { gsap } from "gsap/gsap-core"
 import Button from "../button"
 
 const Hero = ({ title, text, cta }) => {
+  const heroRef = useRef(null)
+
+  useEffect(() => {
+    const title = heroRef.current.querySelector(".h1")
+    const text = heroRef.current.querySelector(".hero__text")
+    const button = heroRef.current.querySelector("a")
+
+    gsap.fromTo(
+      [title, text, button],
+      {
+        opacity: 0,
+        y: 80,
+      },
+      {
+        scrollTrigger: {
+          trigger: heroRef.current,
+          start: () => `top 70%`,
+        },
+        opacity: 1,
+        y: 0,
+        ease: "Quad.easeOut",
+        duration: 1,
+        stagger: {
+          amount: 0.2,
+        },
+      }
+    )
+  }, [])
+
   return (
-    <section className="hero d-center-center">
+    <section className="hero d-center-center" ref={heroRef}>
       <div className="container d-between-center">
         <div className="hero__left">
           <h1 className="h1">{title}</h1>
