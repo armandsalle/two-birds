650ad1d40dea1861a08a789cde1dd16e8cbdbcc5
Armand Sallé <armand.salle@gmail.com>
2020-12-31 12:16:03 +0100

    logo reveal

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useRef, useEffect, useContext } from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
+import { AnimationContext } from "../../contexts/animationContext"
+import reveal from "../../animations/reveal"
 
 const Header = () => {
   const { prismic } = useStaticQuery(
@@ -17,11 +19,19 @@ const Header = () => {
       }
     `
   )
+  const logoRef = useRef()
+  const { animationsCanRuns } = useContext(AnimationContext)
 
   const { siteLogo } = prismic.allLayouts.edges.slice(0, 1).pop().node
 
+  useEffect(() => {
+    if (animationsCanRuns) {
+      reveal(logoRef.current, logoRef.current, false, "70%")
+    }
+  }, [animationsCanRuns])
+
   return (
-    <header className="mt-40 index-header">
+    <header className="mt-40 index-header" ref={logoRef} style={{ opacity: 0 }}>
       <div className="logo">
         <Link to="/">
           <img src={siteLogo?.url} alt="two birds logo" />
